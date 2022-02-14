0545c7fc138b7fc1b38f3e19be417fca21da1e4a
Charlotte <csljeffreys@gmail.com>
2022-02-14 23:38:09 +0000

    Create game

---
+++ script.js
@@ -0,0 +1,98 @@
+let playerOneScore = 0;
+let playerTwoScore = 0;
+let playerOneStatus = '';
+let playerTwoStatus = '';
+
+for (let gameCounter = 1; gameCounter < 8; gameCounter++ ){
+
+    console.log(`THIS IS QUESTION NUMBER ${gameCounter}`);
+    console.log("");
+    
+    let roundResult = prompt ("Did player one win: 1, or player two win: 2, or was it a draw: D?");
+
+
+    if (roundResult == '1'){
+        playerOneScore++;
+        console.log("Player one wins a point");
+        console.log("");
+        console.log(`Player one's score is ${playerOneScore}`);
+        console.log(`Player two's score is ${playerTwoScore}`);
+
+    } else if (roundResult == '2'){
+        playerTwoScore++;
+        console.log("Player two wins a point");
+        console.log("");
+        console.log(`Player one's score is ${playerOneScore}`);
+        console.log(`Player two's score is ${playerTwoScore}`);
+
+    } else if (roundResult == 'D'){
+        console.log("Draw");
+        console.log("");
+        console.log(`Player one's score is ${playerOneScore}`);
+        console.log(`Player two's score is ${playerTwoScore}`);
+
+    } else {
+        console.log("ERROR - RESTART!");
+        break;
+    }
+
+    if (playerOneScore === 3){
+        console.log("");
+        console.log("Player Two is at risk (must win next point)");
+        console.log("PLAYER ONE MATCH POINT");
+    
+        
+    }
+    if (playerTwoScore === 3){
+        console.log("");
+        console.log("Player One is at risk (must win next point)");
+        console.log("PLAYER TWO MATCH POINT");
+        
+    }
+
+    if (playerOneScore === 4){
+        console.log("");
+        console.log("PLAYER ONE WINS");
+        break;
+    }
+    if (playerTwoScore === 4){
+        console.log("");
+        console.log("PLAYER TWO WINS");
+        break;
+    }
+
+    let questionsRemaining = 7 - gameCounter;
+    console.log("");
+    console.log(`There are ${questionsRemaining} questions to go`);  
+    console.log("");      
+    
+    if (questionsRemaining == (playerOneScore - playerTwoScore)){
+        console.log("PLAYER TWO IS AT RISK: MUST WIN ALL REMAINING QUESTIONS");        
+    }
+    if (questionsRemaining == (playerTwoScore - playerOneScore)){
+        console.log("PLAYER ONE IS AT RISK: MUST WIN ALL REMAINING QUESTIONS");        
+    }
+
+    if ((playerOneScore - playerTwoScore)>questionsRemaining){
+        console.log("PLAYER ONE WINS! NOT ENOUGH QUESTIONS LEFT FOR PLAYER TWO TO CATCH UP");
+        break;
+    }
+
+    if ((playerTwoScore - playerOneScore)>questionsRemaining){
+        console.log("PLAYER TWO WINS! NOT ENOUGH QUESTIONS LEFT FOR PLAYER ONE TO CATCH UP");
+        break;
+    }
+
+
+    if (gameCounter == 0){
+        console.log("SUDDEN DEATH");
+    }
+
+
+
+
+
+console.log("");
+
+
+}
